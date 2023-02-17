b59474e7db2b8ad31ad8f3a79a714b278438a7f1
vcedillo99 <64564959+vcedillo99@users.noreply.github.com>
2023-02-17 16:51:30 -0500

    Add files via upload

---
+++ pruebajavascript/src/app.js
@@ -0,0 +1,12 @@
+function View (container) {
+    return {
+        init () {
+            let cont = document.getElementById("content")
+            console.log(cont.offsetHeight, cont.offsetWidth);
+        }
+    }
+}
+
+window.on('', (event) => {
+    
+})
